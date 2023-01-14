727ef60e190260b037b6cc955c4ccbf4807220d0
Tylar <mail@tylar.info>
2023-01-14 17:56:19 -0500

    fix multiple issues
    rm accidental &&. 
    add remote_dir to make DRYer. 
    fix path to script in comment.
    add do_rsync_job function to further DRY things.
    ensure exit code is >0 if any command fails.

--- rsync-cronjobs.sh
+++ rsync-cronjobs.sh
@@ -5,24 +5,42 @@
 # and then NFS is bugged.
 # 
 # This should be added to the root crontab as:
-# 0 7 * * 0 /bin/bash /root/rsync-cronjobs.sh > /root/rsync-cronjob.log
+# 0 7 * * 0 /bin/bash /root/docker_volumes/erddap-config > /root/erddap-rsync-cronjob.log
 
-LOCAL_DIR_01=/mnt/sdb 
-LOCAL_DIR_02=/mnt/sdc
+LOCAL_DIR=/mnt/sdb 
+# LOCAL_DIR_02=/mnt/sdc  # NOTE: currently unused
+REMOTE_DIR=/sr/imars-objects
 
+# these vars just to help create pretty logs
 PREFIX="\t ==="
 SUFFIX="=== === ==="
 
+return_code=0
+do_rsync_job() {
+    # params: 
+    #  $1 : source_location : location to copy from (relative to $REMOTE_DIR) 
+    #  $2 : target_location : location to copy to (absolute).
+    #
+    # example usage: do_rsync_job source_location target_location
+    rysnc -hazv ${REMOTE_DIR}/$1 $2
+    return_code=$(($return_code + $?))  # keep track of exit code
+}
+
+echo "beginning rsync jobs - `date`"
+
 echo "${PREFIX} FK MODA ${SUFFIX}"
-rsync -hazv /srv/imars-objects/fk/MEAN_7D_MODA/OC/* /mnt/sdb/moda_oc_7d_fk/. &&
-rsync -hazv /srv/imars-objects/fk/MEAN_7D_MODA/SST4/* /mnt/sdb/moda_sst4_7d_fk/.
-rsync -hazv /srv/imars-objects/fk/MEAN_7D_MODA/SST/* /mnt/sdb/moda_sst_7d_fk/.
+do_rsync_job fk/MEAN_7D_MODA/OC/*   ${LOCAL_DIR}/moda_oc_7d_fk/. 
+do_rsync_job fk/MEAN_7D_MODA/SST4/* ${LOCAL_DIR}/moda_sst4_7d_fk/.
+do_rsync_job fk/MEAN_7D_MODA/SST/*  ${LOCAL_DIR}/moda_sst_7d_fk/.
 
 echo "${PREFIX} FK VSNPP ${SUFFIX}"
-rsync -hazv /srv/imars-objects/fk/MEAN_7D_VSNPP/OC/* /mnt/sdb/vsnpp_oc_7d_fk/.
-rsync -hazv /srv/imars-objects/fk/MEAN_7D_VSNPP/SSTN/* /mnt/sdb/vsnpp_sstn_7d_fk/.
-rsync -hazv /srv/imars-objects/fk/MEAN_7D_VSNPP/SST/* /mnt/sdb/vsnpp_sst_7d_fk/.
+do_rsync_job fk/MEAN_7D_VSNPP/OC/*   ${LOCAL_DIR}/vsnpp_oc_7d_fk/.
+do_rsync_job fk/MEAN_7D_VSNPP/SSTN/* ${LOCAL_DIR}/vsnpp_sstn_7d_fk/.
+do_rsync_job fk/MEAN_7D_VSNPP/SST/*  ${LOCAL_DIR}/vsnpp_sst_7d_fk/.
 
 echo "${PREFIX} FGB MODA ${SUFFIX}"
-rsync -hazv /srv/imars-objects/fgb/MEAN_7D_MODA/OC/* /mnt/sdb/moda_oc_7d_fgb/.
-rsync -hazv /srv/imars-objects/fgb/MEAN_7D_MODA/SST4/* /mnt/sdb/moda_sst4_7d_fgb/.
+do_rsync_job fgb/MEAN_7D_MODA/OC/*   ${LOCAL_DIR}/moda_oc_7d_fgb/.
+do_rsync_job fgb/MEAN_7D_MODA/SST4/* ${LOCAL_DIR}/moda_sst4_7d_fgb/.
+
+echo "done - `date`"
+exit $return_code
